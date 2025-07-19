ea72def4903d40fd32655a82ab248bd4f2d1e62b
追木智明 <toiki@tsuimokutomoakinoMacBook-Pro.local>
2025-07-20 01:50:39 +0900

    update

--- macos/setup.sh
+++ macos/setup.sh
@@ -6,10 +6,17 @@ fi
 
 brew install bat
 brew install coreutils
-brew install exa
+brew install eza
 brew install ghq
 brew install tig
 brew install httpie
 brew install jq
 brew install anyenv
+brew install gh
+brew install lazydocker
+brew install fzf
 
+anyenv install --init
+anyenv install nodenv
+nodenv install 24.4.1
+nodenv global 24.4.1 
