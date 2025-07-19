29d4229e237237b09ca5f7becded4be019ba62f5
追木智明 <toiki@tsuimokutomoakinoMacBook-Pro.local>
2025-07-20 01:39:40 +0900

    update

--- macos/install.sh
+++ macos/install.sh
@@ -1,14 +1,6 @@
-ln -fs ~/dotfiles/macos/_vimrc ~/.vimrc
-ln -fs ~/dotfiles/macos/_zshrc ~/.zshrc
-ln -fs ~/dotfiles/macos/_zshenv ~/.zshenv
-ln -fs ~/dotfiles/macos/_npmrc ~/.npmrc
-ln -fs ~/dotfiles/macos/_gitconfig ~/.gitconfig
-ln -fs ~/dotfiles/macos/_fzf.zsh ~/.fzf.zsh
-ln -fs ~/dotfiles/macos/_yabairc ~/.yabairc
-ln -fs ~/dotfiles/macos/_skhdrc ~/.skhdrc
-
-cd ~/Library/Application\ Support/Code/User/
-rm settings.json
-ln -s ~/dotfiles/vscode/settings.json ./settings.json
-rm keybindings.json
-ln -s ~/dotfiles/vscode/keybindings.json ./keybindings.json
+ln -fs ~/ghq/github.com/t0iki/dotfiles/macos/_vimrc ~/.vimrc
+ln -fs ~/ghq/github.com/t0iki/dotfiles/macos/_zshrc ~/.zshrc
+ln -fs ~/ghq/github.com/t0iki/dotfiles/macos/_zshenv ~/.zshenv
+# ln -fs ~/dotfiles/macos/_npmrc ~/.npmrc
+# ln -fs ~/dotfiles/macos/_gitconfig ~/.gitconfig
+ln -fs ~/ghq/github.com/t0iki/dotfiles/macos/_fzf.zsh ~/.fzf.zsh
--- macos/setup.sh
+++ macos/setup.sh
@@ -6,17 +6,10 @@ fi
 
 brew install bat
 brew install coreutils
-brew install emacs
 brew install exa
 brew install ghq
-brew install hub
 brew install tig
-brew install tfenv
 brew install httpie
-brew install aws
 brew install jq
-brew install yabai # https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection
-brew install shkd
-brew install fig
 brew install anyenv
 
